79cd15f70ef067f8cdc3bc3a62b6985b5b5c0cf5
Zachary Kirtz <zach.kirtz@gmail.com>
2026-08-08 00:09:35 -0500

    hio: Add hio error codes.

---
+++ Test/hio/ErrorCodes.cs
@@ -0,0 +1,24 @@
+
+namespace Test.hio;
+
+public enum ErrorCodes {
+    SuccessContinue         = 0,
+    SuccessEnd              = 1,
+    AllocationFailed        = 2,
+    DirectoryNotEmpty       = 3,
+    DirectoryStatusChanged  = 4,
+    EntryCorrupted          = 5,
+    FileCorrupted           = 6,
+    FileDataCorrupted       = 7,
+    Unknown8                = 8,
+    Unknown9                = 9,
+    Unknown10               = 10,
+    Unknown11               = 11,
+    OutOfRange              = 12,
+    PathAlreadyExists       = 13,
+    PathNotFound            = 14,
+    TargetLocked            = 15,
+    Unknown16               = 16,
+    UsableSpaceNotEnough    = 17
+}
+    
